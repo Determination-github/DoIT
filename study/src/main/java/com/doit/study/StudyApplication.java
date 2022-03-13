e52eec75e3f2de8cf839158f87df2d366f1c6e7f
Determination-github <ungseopkim@naver.com>
2022-03-13 19:58:57 +0900

    feat 마이바티스 환경설정 및 OJDBC8 라이브러리 추가

--- study/src/main/java/com/doit/study/StudyApplication.java
+++ study/src/main/java/com/doit/study/StudyApplication.java
@@ -1,8 +1,10 @@
 package com.doit.study;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan(value = {"com.doit.study.mapper"})
 @SpringBootApplication
 public class StudyApplication {
 
---
+++ study/src/main/java/com/doit/study/mapper/MemberMapper.java
@@ -0,0 +1,7 @@
+package com.doit.study.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public class MemberMapper {
+}
