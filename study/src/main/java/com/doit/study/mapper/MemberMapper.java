bd77eb8928b408f3b3a6c48b6823530c1fe1e74f
Determination-github <ungseopkim@naver.com>
2022-03-31 17:21:11 +0900

    feat 소셜 회원 회원가입 용 mapper 인터페이스 기능 구현

--- study/src/main/java/com/doit/study/mapper/MemberMapper.java
+++ study/src/main/java/com/doit/study/mapper/MemberMapper.java
@@ -2,6 +2,7 @@ package com.doit.study.mapper;
 
 
 import com.doit.study.member.domain.Member;
+import com.doit.study.member.domain.Social;
 import org.apache.ibatis.annotations.*;
 
 import java.util.Optional;
@@ -13,12 +14,20 @@ public interface MemberMapper {
     @Insert(MemberSQL.insert)
     Integer insert(@Param("member") Member member);
 
+    //Social 회원가입
+    @Insert(MemberSQL.insertSocial)
+    Integer insertSocial(@Param("social") Social social);
+
     //이메일로 아이디 찾기
     @Select(MemberSQL.findByEmail)
     @Results({
             @Result(property = "user_id", column = "USER_ID")
     })
     Optional<Member> findByEmail(@Param("email") String email);
+
+    @Select(MemberSQL.findBySocialId)
+    Optional<Social> findSocialMemberById(@Param("user_id") String id);
+
 
     //닉네임 중복 체크
     @Select(MemberSQL.checkNickname)
