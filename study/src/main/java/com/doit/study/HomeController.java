1c2853353c1fa76468dfa953ea72e44691b8166a
Determination-github <ungseopkim@naver.com>
2022-03-31 17:17:17 +0900

    chore 홈 컨트롤러 리팩토링

--- study/src/main/java/com/doit/study/HomeController.java
+++ study/src/main/java/com/doit/study/HomeController.java
@@ -8,9 +8,7 @@ import com.doit.study.member.dto.NaverDto;
 import com.doit.study.member.service.MemberService;
 
 import com.doit.study.board.domain.Pagination;
-import com.doit.study.board.domain.SearchCondition;
 import com.doit.study.board.dto.BoardDto;
-import com.doit.study.board.dto.SearchBoardDto;
 import com.doit.study.board.service.BoardService;
 
 import lombok.RequiredArgsConstructor;
@@ -21,6 +19,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Controller
 @Slf4j
@@ -32,66 +31,39 @@ public class HomeController {
     private final BoardService boardService;
 
     @GetMapping
-//    @ResponseBody
     public String home(
-            @SessionAttribute(name = SessionConst.NAVER_MEMBER, required = false) NaverDto naverDto,
-            @SessionAttribute(name = SessionConst.KAKAO_MEMBER, required = false) KakaoDto kakaoDto,
-            @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) LoginDto loginDto,
-            Model model) {
+            HttpSession session,
+            @RequestParam(value = "currentPage", required = false, defaultValue = "1") int currentPage,
+            @RequestParam(value = "pageSize", required = false, defaultValue = "4") int pageSize,
+            Model model) throws Exception {
 
-        //네이버로 로그인 했는데 회원정보가 없는 경우
-        if (naverDto != null) {
-            String naverEmail = naverDto.getNaverEmail();
-            if (memberService.findSocialMember(naverEmail) == null) {
-                log.info("회원가입이 필요합니다.");
-                model.addAttribute("naverMember", naverDto);
-                return "redirect:/members/join";
-            }
+        Object naverDto = session.getAttribute(SessionConst.NAVER_MEMBER);
+        Object kakaoDto = session.getAttribute(SessionConst.KAKAO_MEMBER);
+        Object memberDto = session.getAttribute(SessionConst.LOGIN_MEMBER);
+
+        log.info("naverDto = " + naverDto);
+        log.info("kakaoDto = " + kakaoDto);
+        log.info("memberDto = " + memberDto);
+
+        if(naverDto != null) {
+            session.setAttribute("naverDto", naverDto);
+        } else if(kakaoDto != null) {
+            session.setAttribute("kakaoDto", kakaoDto);
+        } else if(memberDto != null) {
+            session.setAttribute("memberDto", memberDto);
         }
-
-        if (kakaoDto != null) {
-            String kakaoEmail = kakaoDto.getKakaoEmail();
-            if (kakaoEmail == null) {
-                log.info("회원가입이 필요합니다. - 이메일 정보가 없음");
-                model.addAttribute("kakaoMember", kakaoDto);
-                return "redirect:/members/join";
-            }
-
-            if (kakaoEmail != null) {
-                if (memberService.findSocialMember(kakaoEmail) == null) {
-                    log.info("회원가입이 필요합니다.");
-                    model.addAttribute("kakaoMember", kakaoDto);
-                    return "redirect:/members/join";
-                }
-            }
-        }
-
-        if (loginDto == null && naverDto == null && kakaoDto == null) {
-            log.info("회원정보가 없습니다.");
-            return "Home";
-        }
-
-        //loginDto의 값이 있는 경우(로그인 한 경우)
-        model.addAttribute("member", loginDto);
-        log.info("로그인 성공");
-        return "index";
-    }
-
-    @GetMapping
-    public String list(@RequestParam(value = "currentPage", required = false, defaultValue = "1") int currentPage,
-                       @RequestParam(value = "pageSize", required = false, defaultValue = "4") int pageSize,
-                       Model m, HttpServletRequest request) throws Exception {
-//        if(!loginCheck(request))
-//            return "redirect:/login/login?toURL="+request.getRequestURL();
 
         BoardDto boardDto = new BoardDto();
         int totalRecordCount = boardService.getCount();
         Pagination pagination = new Pagination(currentPage, pageSize);
         pagination.setTotalRecordCount(totalRecordCount);
 
-        m.addAttribute("pagination", pagination);
-        m.addAttribute("list", boardService.getPage(pagination));
-        m.addAttribute("board", boardDto);
+        log.info("totalRecordCount = " + totalRecordCount);
+        model.addAttribute("pagination", pagination);
+        log.info("pagination = " + pagination);
+        model.addAttribute("lists", boardService.getPage(pagination));
+        log.info("lists = " + boardService.getPage(pagination));
+        model.addAttribute("board", boardDto);
         return "/index2";
     }
 }
