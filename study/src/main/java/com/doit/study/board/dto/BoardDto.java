37afdbb64d2e5b59cdb3017ef500ef25250e103a
Determination-github <ungseopkim@naver.com>
2022-05-04 00:56:31 +0900

    refactor 게시판 관련 클래스 리팩토링 및 AWS S3 적용을 위한 코드 수정

--- study/src/main/java/com/doit/study/board/dto/BoardDto.java
+++ study/src/main/java/com/doit/study/board/dto/BoardDto.java
@@ -1,147 +1,97 @@
 package com.doit.study.board.dto;
 
-import lombok.*;
+import com.doit.study.board.domain.Board;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
-import java.util.Map;
 
 @Data
-@AllArgsConstructor
-public class BoardDto {
-    private int     board_id;
-    private String  board_writerId;
-    private String  board_title;
-    private String  board_subTitle;
-    private String  board_content;
-    private String  board_location;
-    private String  board_onOffline;
-    private int     board_viewCount;
-    private int     board_commentCount;
-    private String  board_startDate;
-    private String  board_endDate;
-    private Date    board_regDate;
-    private String  board_hashtag;
+@NoArgsConstructor
+public class BoardWriteDto {
+    private String  board_id, board_writerId;
+    private String  board_startDate, board_endDate;
+    private String  board_title, board_subTitle, board_content;
+    private String  board_location, board_hashtag;
+    private int     board_viewCount, board_commentCount, board_onOffline;
+    private String  writer_nickName;
+    private String  writer_interest1, writer_interest2, writer_interest3;
+    private Date board_regDate;
 
-//
-//    public BoardDto(String board_writerId, String board_startDate, String board_endDate,
-//                    String board_location, String board_onOffline, String board_subTitle) {
-//        this.board_writerId = board_writerId;
-//        this.board_startDate = board_startDate;
-//        this.board_endDate = board_endDate;
-//        this.board_location = board_location;
-//        this.board_onOffline = board_onOffline;
-//        this.board_subTitle = board_subTitle;
-//    }
-//
-//    public BoardDto fsDtoToBoardDto(FirstStudyDto firstStudyDto) {
-//        BoardDto boardDto = new BoardDto();
-//        this.board_writerId = firstStudyDto.getWriterId();
-//        this.board_startDate = firstStudyDto.getStartDate();
-//        this.board_endDate = firstStudyDto.getEndDate();
-//        this.board_location = firstStudyDto.getLocation();
-//        this.board_onOffline = firstStudyDto.getOnOffLine();
-//        this.board_subTitle = firstStudyDto.getSubTitle();
-//        return boardDto;
-//    }
+    //첫 번째 뷰페이지에서 넘어온 데이터 합치기
+    public BoardWriteDto toBoardWriteDto(FirstStudyDto firstStudyDto, BoardWriteDto boardWriteDto) {
 
-    private String  board_File;
-    private String  board_FilePath;
+        //boardWriteDto 값 세팅
+        boardWriteDto.setWriter_nickName(firstStudyDto.getNickName());
+        boardWriteDto.setWriter_interest1(firstStudyDto.getInterest1());
+        boardWriteDto.setWriter_interest2(firstStudyDto.getInterest2());
+        boardWriteDto.setWriter_interest3(firstStudyDto.getInterest3());
+        boardWriteDto.setBoard_writerId(firstStudyDto.getWriterId());
+        boardWriteDto.setBoard_startDate(firstStudyDto.getStartDate());
+        boardWriteDto.setBoard_endDate(firstStudyDto.getEndDate());
+        boardWriteDto.setBoard_location(firstStudyDto.getTotalLocation());
+        boardWriteDto.setBoard_onOffline(firstStudyDto.getFlag());
+        boardWriteDto.setBoard_subTitle(firstStudyDto.getSubTitle());
 
-    private Integer currentPage = 1;
-    private Integer pageSize = 4;
-    private Integer firstRecordIndex = 1;
-    private Integer lastRecordIndex = 3;
-    // 한 페이지당 게시물 갯수
-    public final int countPerPage = 3;
-    // 전체 데이터 개수
-    private int totalRecordCount;
-    // 전체 페이지 개수
-    private int totalPageCount;
-    // 페이지 리스트의 첫 페이지 번호
-    private int firstPage;
-    // 페이지 리스트의 마지막 페이지 번호
-    private int lastPage;
-    // 이전 페이지 존재 여부
-    private boolean hasPreviousPage;
-    // 다음 페이지 존재 여부
-    private boolean hasNextPage;
-
-    public BoardDto(){
-        new BoardDto(1, 4);
+        return boardWriteDto;
     }
 
-    public BoardDto(int currentPage, int pageSize) {
-        //강제입력방지
-        if (currentPage < 1) {
-            currentPage = 1;
-        }
-        // 하단 페이지 갯수 4개로 제한
-        if (pageSize != 4) {
-            pageSize = 4;
-        }
-        this.currentPage = currentPage;
-        this.pageSize = pageSize;
-        this.calculation();
+    public Board toEntity(String uuid, BoardWriteDto boardWriteDto) {
+        return Board.builder()
+                .study_id(uuid)
+                .user_id(board_writerId)
+                .schedule_start(board_startDate)
+                .schedule_end(board_endDate)
+                .title(board_title)
+                .content(board_content)
+                .address(board_location)
+                .interest1(writer_interest1)
+                .interest2(writer_interest2)
+                .interest3(writer_interest3)
+                .sub_title(board_subTitle)
+                .moim_flag(board_onOffline)
+                .build();
     }
 
-    public BoardDto(int totalRecordCount) {
-        this.totalRecordCount = totalRecordCount;
-
-        doPaging(totalRecordCount);
+    @Builder
+    public BoardWriteDto(String board_id, String board_writerId, String board_startDate, String board_endDate, String board_title, String board_subTitle, String board_content, String board_location, String board_hashtag, int board_viewCount, int board_commentCount, int board_onOffline, String writer_nickName, String writer_interest1, String writer_interest2, String writer_interest3, Date board_regDate) {
+        this.board_id = board_id;
+        this.board_writerId = board_writerId;
+        this.board_startDate = board_startDate;
+        this.board_endDate = board_endDate;
+        this.board_title = board_title;
+        this.board_subTitle = board_subTitle;
+        this.board_content = board_content;
+        this.board_location = board_location;
+        this.board_hashtag = board_hashtag;
+        this.board_viewCount = board_viewCount;
+        this.board_commentCount = board_commentCount;
+        this.board_onOffline = board_onOffline;
+        this.writer_nickName = writer_nickName;
+        this.writer_interest1 = writer_interest1;
+        this.writer_interest2 = writer_interest2;
+        this.writer_interest3 = writer_interest3;
+        this.board_regDate = board_regDate;
     }
 
-
-    public void doPaging(int totalRecordCount) {
-        this.totalRecordCount = totalRecordCount;
-
-        if (totalRecordCount > 0) {
-            this.calculation();
-        }
+    public BoardWriteDto toBoardWriteDto(Board board) {
+        return BoardWriteDto.builder()
+                .board_id(board.getStudy_id())
+                .board_writerId(board.getUser_id())
+                .board_startDate(board.getSchedule_start())
+                .board_endDate(board.getSchedule_end())
+                .board_title(board.getTitle())
+                .board_content(board.getContent())
+                .board_subTitle(board.getSub_title())
+                .board_location(board.getAddress())
+                .board_regDate(board.getReg_date())
+                .board_onOffline(board.getMoim_flag())
+                .board_viewCount(board.getView_count())
+                .board_commentCount(board.getComment_count())
+                .writer_interest1(board.getInterest1())
+                .writer_interest2(board.getInterest2())
+                .writer_interest3(board.getInterest3())
+                .build();
     }
-
-
-    public void calculation() {
-
-        // 전체 페이지 수 (현재 페이지 번호가 전체 페이지 수보다 크면 현재 페이지 번호에 전체 페이지 수를 저장)
-        this.totalPageCount = ((totalRecordCount - 1) / this.getPageSize()) + 1;
-        if (this.getCurrentPage() > totalPageCount) {
-            this.setCurrentPage(totalPageCount);
-        }
-
-        // 페이지 리스트의 첫 페이지 번호
-        firstPage = ((this.getCurrentPage() - 1) / this.getPageSize()) * this.getPageSize() + 1;
-
-        // 페이지 리스트의 마지막 페이지 번호 (마지막 페이지가 전체 페이지 수보다 크면 마지막 페이지에 전체 페이지 수를 저장)
-        lastPage = firstPage + this.getPageSize() - 1;
-        if (lastPage > totalPageCount) {
-            lastPage = totalPageCount;
-        }
-
-        // SQL의 조건절에 사용되는 첫 RNUM
-        firstRecordIndex = (this.getCurrentPage() - 1) * countPerPage; //3
-        // SQL의 조건절에 사용되는 마지막 RNUM
-        lastRecordIndex = this.getCurrentPage() * countPerPage;
-
-        // 이전 페이지 존재 여부
-        hasPreviousPage = firstPage == 1 ? false : true;
-        if (hasPreviousPage == false) {
-            if (currentPage != firstPage) {
-                hasPreviousPage = true;
-            } else {
-                hasPreviousPage = false;
-            }
-        }
-
-        // 다음 페이지 존재 여부
-        hasNextPage = (lastPage * countPerPage) >= totalRecordCount ? false : true;
-        if(hasNextPage == false) {
-            if(currentPage != lastPage) {
-                hasNextPage = true;
-            }else {
-                hasNextPage = false;
-            }
-        }
-    }
-
 }
-
