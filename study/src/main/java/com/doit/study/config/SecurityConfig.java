f0c36b47b6375ccb2f827351f278eb5db0f3c627
Determination-github <ungseopkim@naver.com>
2022-06-14 21:48:16 +0900

    feat BCrypt로 비밀번호 암호화 기능 추가

---
+++ study/src/main/java/com/doit/study/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.doit.study.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
