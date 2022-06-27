0cdc06da624a816f563196d46d5013b5df37c40a
ungseopkim@naver.com <ungseopkim@naver.com>
2022-06-27 18:58:55 +0900

    docs deploy.sh repository 위치 변경

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-REPOSITORY=/home/ec2-user/app
+REPOSITORY=/home/ec2-user/app/study
 cd $REPOSITORY
 
 APP_NAME=study #1
@@ -17,4 +17,4 @@ else
 fi
 
 echo "> $JAR_PATH 배포" #3
-nohup java -jar /home/ec2-user/app/build/libs/study-0.0.1-SNAPSHOT.jar --spring.config.location=/home/ec2-user/application.yml > /dev/null 2> /dev/null < /dev/null &
+nohup java -jar /home/ec2-user/app/study/build/libs/study-0.0.1-SNAPSHOT.jar --spring.config.location=/home/ec2-user/application.yml > /dev/null 2> /dev/null < /dev/null &
