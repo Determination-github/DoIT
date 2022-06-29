6212ae0fa25c5236dda3fabf62ec176f474ba8ce
ungseopkim@naver.com <ungseopkim@naver.com>
2022-06-29 16:43:46 +0900

    fix config 설정 변경

--- src/main/java/com/doit/study/config/SecurityConfig.java
+++ src/main/java/com/doit/study/config/SecurityConfig.java
@@ -13,6 +13,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
+        http.headers().frameOptions().sameOrigin();
     }
 
     @Bean
