183abf22b8de4f8e0c9278833e3b1903ca7aafb7
ungseopkim@naver.com <ungseopkim@naver.com>
2022-07-01 23:34:38 +0900

    docs InterceptorConfig 수정

--- src/main/java/com/doit/study/config/InterceptorConfig.java
+++ src/main/java/com/doit/study/config/InterceptorConfig.java
@@ -19,8 +19,9 @@ public class InterceptorConfig implements WebMvcConfigurer {
         registry.addInterceptor(new LoginCheckInterceptor(alarmService))
                 .order(1)
                 .addPathPatterns("/**")
-                .excludePathPatterns("/", "/join", "/login", "/logout", "/kakao/**", "/naver/**", "/favicon.ico", "/health",
-                        "/board/result/**", "/css/**", "/img/**", "/js/**", "/join/**", "/login/**", "/error", "/board/list/**");
+                .excludePathPatterns("/", "/join", "/login", "/logout", "/kakao/**", "/naver/**", "/favicon.ico",
+                        "/health", "/board/result/**", "/css/**", "/img/**", "/js/**", "/join/**", "/login/**",
+                        "/error", "/board/list/**", "/lib/sockjs-client/**", "/websocket/**");
 
         registry.addInterceptor(new JoinCheckInterceptor())
                 .order(2)
