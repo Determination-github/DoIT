437073ec1c709f36050fa7322588ca3d1892c908
ungseopkim@naver.com <ungseopkim@naver.com>
2022-07-07 20:57:03 +0900

    docs 알람 정렬 방식 변경

--- src/main/java/com/doit/study/mapper/AlarmSQL.java
+++ src/main/java/com/doit/study/mapper/AlarmSQL.java
@@ -8,7 +8,7 @@ public class AlarmSQL {
 
     public static final String getAlarm =
             "SELECT * FROM ALARM_TB WHERE user_id = #{user_id} " +
-                    "ORDER BY reg_date DESC";
+                    "ORDER BY reg_date ASC";
 
     public static final String deleteAlarm =
             "DELETE FROM ALARM_TB " +
