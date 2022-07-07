d18f9d4bb5593aaaaa49efd4cf8c20b0e165783e
ungseopkim@naver.com <ungseopkim@naver.com>
2022-07-07 16:37:09 +0900

    docs StudyApplication PropertySource 추가

--- src/main/java/com/doit/study/StudyApplication.java
+++ src/main/java/com/doit/study/StudyApplication.java
@@ -8,8 +8,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.PropertySource;
 
 @MapperScan(value = {"com.doit.study.mapper"})
+@PropertySource(value = {"classpath:database_pwd.properties", "classpath:database_id.properties"})
 @SpringBootApplication
 public class StudyApplication {
 
