8cb0caa8bf2759227e9cc2e2f435e4e963f7edb0
ungseopkim@naver.com <ungseopkim@naver.com>
2022-07-13 22:06:17 +0900

    refact KakaoServiceImpl & NaverServiceImpl 패키지 경로 변경

---
+++ src/main/java/com/doit/study/member/service/social/KakaoServiceImpl.java
@@ -0,0 +1,232 @@
+package com.doit.study.member.service.social;
+
+import com.doit.study.mapper.MemberMapper;
+import com.doit.study.member.domain.Member;
+import com.doit.study.member.dto.MemberDto;
+import com.doit.study.member.naver.KakaoLoginApi;
+import com.doit.study.member.service.SocialService;
+import com.github.scribejava.core.builder.ServiceBuilder;
+import com.github.scribejava.core.model.OAuth2AccessToken;
+import com.github.scribejava.core.model.OAuthRequest;
+import com.github.scribejava.core.model.Response;
+import com.github.scribejava.core.model.Verb;
+import com.github.scribejava.core.oauth.OAuth20Service;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import javax.servlet.http.HttpSession;
+import java.io.*;
+import java.math.BigInteger;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.security.SecureRandom;
+import java.util.HashMap;
+import java.util.Optional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@PropertySource("classpath:login.properties")
+@PropertySource("classpath:kakao_secret.properties")
+public class KakaoServiceImpl implements SocialService {
+
+    private final MemberMapper memberMapper;
+
+    @Value("${kakao.login.client.id}")
+    private String CLIENT_ID;
+
+    @Value("${kakao.login.client.secret}")
+    private String CLIENT_SECRET;
+
+    @Value("${kakao.login.redirect.url}")
+    private String REDIRECT_URI;
+
+    @Value("${kakao.login.session.state}")
+    private String SESSION_STATE;
+
+    // 프로필 조회 API URL
+    @Value("${kakao.login.profile.api.url}")
+    private String PROFILE_API_URL;
+
+    /**
+     * session에 데이터 저장
+     * @param session
+     * @param state
+     */
+    private void setSession(HttpSession session, String state) {
+        session.setAttribute(SESSION_STATE, state);
+    }
+
+    /**
+     * 세션에서 데이터 가져오기
+     * @param session
+     * @return String
+     */
+    private String getSession(HttpSession session) {
+        return (String) session.getAttribute(SESSION_STATE);
+    }
+
+    /**
+     * State 생성
+     * @return String
+     */
+    public String generateState()
+    {
+        SecureRandom random = new SecureRandom();
+        return new BigInteger(130, random).toString(32);
+    }
+
+    /**
+     * 카카오 아이디로 인증 URL 생성
+     * @param session
+     * @return
+     */
+    @Override
+    public String getAuthorizationUrl(HttpSession session) {
+        String state = generateState();
+
+        setSession(session, state);
+
+        OAuth20Service oauthService = new ServiceBuilder()
+                .apiKey(CLIENT_ID)
+                .apiSecret(CLIENT_SECRET)
+                .callback(REDIRECT_URI)
+                .state(state)
+                .build(KakaoLoginApi.instance());
+
+        return oauthService.getAuthorizationUrl();
+    }
+
+    /**
+     * 카카오 아이디로 Callback 처리 및 AccessToken 획득
+     * @param session
+     * @param code
+     * @param state
+     * @return OAuth2AccessToken
+     * @throws IOException
+     */
+    public OAuth2AccessToken getAccessToken(HttpSession session, String code, String state) throws IOException {
+
+        // Callback으로 전달받은 세선검증용 난수값과 세션에 저장되어있는 값이 일치하는지 확인
+        String sessionState = getSession(session);
+        if (StringUtils.pathEquals(sessionState, state)) {
+
+            OAuth20Service oauthService = new ServiceBuilder()
+                    .apiKey(CLIENT_ID)
+                    .apiSecret(CLIENT_SECRET)
+                    .callback(REDIRECT_URI)
+                    .state(state)
+                    .build(KakaoLoginApi.instance());
+
+            // Scribe에서 제공하는 AccessToken 획득 기능으로 카카오 Access Token을 획득
+            OAuth2AccessToken accessToken = oauthService.getAccessToken(code);
+            return accessToken;
+        }
+        return null;
+    }
+
+    /**
+     * 카카오 로그인 회원 정보 가져오기
+     * @param access_Token
+     * @return HashMap<String, String>
+     * @throws ParseException
+     */
+    @Override
+    public HashMap<String, String> getSocialUserInfo(String access_Token) throws ParseException, IOException {
+
+        //요청하는 클라이언트마다 가진 정보가 다를 수 있기에 HashMap타입으로 선언
+        HashMap<String, String> userInfo = new HashMap<String, String>();
+        String reqURL = "https://kapi.kakao.com/v2/user/me";
+
+        URL url = new URL(reqURL);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+
+        //요청에 필요한 Header에 포함될 내용
+        conn.setRequestProperty("Authorization", "Bearer " + access_Token);
+
+        int responseCode = conn.getResponseCode();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+        String line = "";
+        String result = "";
+
+        while ((line = br.readLine()) != null) {
+            result += line;
+        }
+
+        JSONParser parser = new JSONParser();
+        Object obj = parser.parse(result);
+        JSONObject jsonObj = (JSONObject) obj;
+
+        //고유 카카오 회원 아이디
+        Long id = (Long) jsonObj.get("id");
+
+        JSONObject properties_obj = (JSONObject)jsonObj.get("properties");
+        JSONObject kakao_account_obj = (JSONObject)jsonObj.get("kakao_account");
+
+        //회원정보 가져오기
+        String nickname = (String)properties_obj.get("nickname");
+        String email = (String)kakao_account_obj.get("email");
+        String gender = (String)kakao_account_obj.get("gender");
+
+        //회원정보 저장
+        userInfo.put("accessToken", access_Token);
+        userInfo.put("id", String.valueOf(id));
+        userInfo.put("nickname", nickname);
+        userInfo.put("email", email);
+        userInfo.put("gender", gender);
+
+        return userInfo;
+    }
+
+    /**
+     * 카카오 회원 정보 삭제
+     * @param access_Token
+     */
+    @Override
+    public void deleteAccessToken(String access_Token) throws IOException {
+        String reqURL = "https://kapi.kakao.com/v1/user/unlink";
+
+        //url 설정
+        URL url = new URL(reqURL);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Authorization", "Bearer " + access_Token);
+        int responseCode = conn.getResponseCode();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String result = "";
+        String line = "";
+
+        while ((line = br.readLine()) != null) {
+            result += line;
+        }
+    }
+
+    /**
+     * 카카오 로그인한 회원 정보 찾기
+     * @param id
+     * @return MemberDto
+     */
+    @Override
+    public MemberDto findSocialMember(String id) {
+        //member객체 찾기
+        Optional<Member> findMember = memberMapper.findSocialMemberBySocialId(id);
+
+        if(findMember.isPresent()) { //있으면 member 객체 반환
+            Member member = findMember.get();
+            return new MemberDto().toDto(member);
+        } else {
+            return null;
+        }
+    }
+}
---
+++ src/main/java/com/doit/study/member/service/social/NaverServiceImpl.java
@@ -0,0 +1,224 @@
+package com.doit.study.member.service.social;
+
+import com.doit.study.mapper.MemberMapper;
+import com.doit.study.member.domain.Member;
+import com.doit.study.member.dto.MemberDto;
+import com.doit.study.member.naver.NaverLoginApi;
+import com.doit.study.member.service.SocialService;
+import com.github.scribejava.core.builder.ServiceBuilder;
+import com.github.scribejava.core.model.OAuth2AccessToken;
+import com.github.scribejava.core.model.OAuthRequest;
+import com.github.scribejava.core.model.Response;
+import com.github.scribejava.core.model.Verb;
+import com.github.scribejava.core.oauth.OAuth20Service;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.security.SecureRandom;
+import java.util.HashMap;
+import java.util.Optional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@PropertySource("classpath:login.properties")
+@PropertySource("classpath:naver_secret.properties")
+public class NaverServiceImpl implements SocialService {
+
+    private final MemberMapper memberMapper;
+
+    @Value("${naver.login.client.id}")
+    private String CLIENT_ID;
+
+    @Value("${naver.login.client.secret}")
+    private String CLIENT_SECRET;
+
+    @Value("${naver.login.redirect.url}")
+    private String REDIRECT_URI;
+
+    @Value("${naver.login.session.state}")
+    private String SESSION_STATE;
+
+    // 프로필 조회 API URL
+    @Value("${naver.login.profile.api.url}")
+    private String PROFILE_API_URL;
+
+    /**
+     * session에 데이터 저장
+     * @param session
+     * @param state
+     */
+    private void setSession(HttpSession session, String state){
+        session.setAttribute(SESSION_STATE, state);
+    }
+
+    /**
+     * 세션에서 데이터 가져오기
+     * @param session
+     * @return String
+     */
+    private String getSession(HttpSession session){
+        return (String) session.getAttribute(SESSION_STATE);
+    }
+
+    /**
+     * State 생성
+     * @return String
+     */
+    public String generateState()
+    {
+        SecureRandom random = new SecureRandom();
+        return new BigInteger(130, random).toString(32);
+    }
+
+    /**
+     * 네이버 아이디로 인증 URL 생성
+     * @param session
+     * @return String
+     */
+    @Override
+    public String getAuthorizationUrl(HttpSession session) {
+
+        /* 세션 유효성 검증을 위하여 난수를 생성 */
+        String state = generateState();
+        
+        /* 생성한 난수 값을 session에 저장 */
+        setSession(session,state);
+
+        /* Scribe에서 제공하는 인증 URL 생성 기능을 이용하여 네아로 인증 URL 생성 */
+        OAuth20Service oauthService = new ServiceBuilder()
+                .apiKey(CLIENT_ID)
+                .apiSecret(CLIENT_SECRET)
+                .callback(REDIRECT_URI)
+                .state(state) //앞서 생성한 난수값을 인증 URL생성시 사용함
+                .build(NaverLoginApi.instance());
+
+        //인증 URL 리턴
+        return oauthService.getAuthorizationUrl();
+    }
+
+    /**
+     * 네이버 아이디로 Callback 처리 및 AccessToken 획득
+     * @param session
+     * @param code
+     * @param state
+     * @return OAuth2AccessToken
+     * @throws IOException
+     */
+    @Override
+    public OAuth2AccessToken getAccessToken(HttpSession session, String code, String state) throws IOException {
+
+        // Callback으로 전달받은 세선검증용 난수값과 세션에 저장되어있는 값이 일치하는지 확인
+        String sessionState = getSession(session);
+        if(StringUtils.pathEquals(sessionState, state)){
+
+            OAuth20Service oauthService = new ServiceBuilder()
+                    .apiKey(CLIENT_ID)
+                    .apiSecret(CLIENT_SECRET)
+                    .callback(REDIRECT_URI)
+                    .state(state)
+                    .build(NaverLoginApi.instance());
+
+            // Scribe에서 제공하는 AccessToken 획득 기능으로 네아로 Access Token을 획득
+            OAuth2AccessToken accessToken = oauthService.getAccessToken(code);
+            return accessToken;
+        }
+        return null;
+    }
+
+    /**
+     * Access Token을 이용하여 네이버 사용자 프로필 API를 호출
+     * @param oauthToken
+     * @return String
+     * @throws IOException
+     */
+    @Override
+    public String getUserProfile(OAuth2AccessToken oauthToken) throws IOException{
+
+        //OAuth20Service 객체 정보 설정
+        OAuth20Service oauthService = new ServiceBuilder()
+                .apiKey(CLIENT_ID)
+                .apiSecret(CLIENT_SECRET)
+                .callback(REDIRECT_URI)
+                .build(NaverLoginApi.instance());
+
+        //OAuthRequest 응답 객체 생성
+        OAuthRequest request = new OAuthRequest(Verb.GET, PROFILE_API_URL, oauthService);
+        oauthService.signRequest(oauthToken, request);
+
+        //정보 전송
+        Response response = request.send();
+        return response.getBody();
+    }
+
+    /**
+     * 네이버 로그인 회원 정보 가져오기
+     * @param apiResult
+     * @return HashMap<String, String>
+     * @throws ParseException
+     */
+    @Override
+    public HashMap<String, String> getSocialUserInfo(String apiResult) throws ParseException {
+        JSONParser parser = new JSONParser();
+        Object obj = parser.parse(apiResult);
+        JSONObject jsonObj = (JSONObject) obj;
+        JSONObject response_obj = (JSONObject)jsonObj.get("response");
+
+        //회원정보 가져오기
+        String id = (String)response_obj.get("id");
+        String email = (String)response_obj.get("email");
+        String gender = (String)response_obj.get("gender");
+        String name = (String)response_obj.get("name");
+
+        HashMap<String, String> userInfo = new HashMap<>();
+
+        //회원정보 저장
+        userInfo.put("id", id);
+        userInfo.put("name", name);
+        userInfo.put("email", email);
+        userInfo.put("gender", gender);
+
+        return userInfo;
+    }
+
+    /***
+     * 네이버 회원 정보 삭제
+     * @param accessToken
+     */
+    @Override
+    public void deleteAccessToken(String accessToken) {
+        String deleteUrl =
+                "https://nid.naver.com/oauth2.0/token?grant_type=delete"
+                        +   "&client_id=" + CLIENT_ID
+                        +   "&client_secret=" + CLIENT_SECRET
+                        +   "&access_token=" + accessToken
+                        +   "&service_provider=NAVER";
+        //result : success
+    }
+
+    /**
+     * 네이버 로그인한 회원 정보 찾기
+     * @param id
+     * @return MemberDto
+     */
+    @Override
+    public MemberDto findSocialMember(String id) {
+        Optional<Member> findMember = memberMapper.findSocialMemberBySocialId(id);
+
+        if(findMember.isPresent()) {
+            Member member = findMember.get();
+            return new MemberDto().toDto(member);
+        }
+        return null;
+    }
+}
