ec41d705579e34a24e8a314eb48d37934fe96867
ungseopkim@naver.com <ungseopkim@naver.com>
2022-07-13 21:58:01 +0900

    docs JoinCheckIntreceptor에서 사용하지 않는 코드 삭제

--- src/main/java/com/doit/study/interceptor/JoinCheckInterceptor.java
+++ src/main/java/com/doit/study/interceptor/JoinCheckInterceptor.java
@@ -14,10 +14,6 @@ public class JoinCheckInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
-        String requestURI = request.getRequestURI();
-
-        log.info("인증 체크 인터셉터 실행 {}", requestURI);
-
         HttpSession session = request.getSession(false);
 
         if (session != null &&
