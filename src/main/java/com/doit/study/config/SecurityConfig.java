cea4970736563622e0c5093f60cba50d14d81c15
ungseopkim@naver.com <ungseopkim@naver.com>
2022-07-15 14:19:23 +0900

    docs csrf 옵션 활성화

--- src/main/java/com/doit/study/config/SecurityConfig.java
+++ src/main/java/com/doit/study/config/SecurityConfig.java
@@ -12,7 +12,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
         http.headers().frameOptions().sameOrigin();
     }
 
