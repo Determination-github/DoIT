a30ddda5e3a0bb6e04471f692e2fcd294efe2e82
ungseopkim@naver.com <ungseopkim@naver.com>
2022-07-15 17:53:38 +0900

    docs sameorigin 옵션에 따른 결과 비교를 위해 임시 주석 처리

--- src/main/java/com/doit/study/config/SecurityConfig.java
+++ src/main/java/com/doit/study/config/SecurityConfig.java
@@ -14,8 +14,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.headers()
-                .frameOptions()
-                .sameOrigin()
             .and()
                 .csrf()
                 .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
