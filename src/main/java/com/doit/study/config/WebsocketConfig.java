31c3d4aabc2dd6158db984573f8390e20a655d98
ungseopkim@naver.com <ungseopkim@naver.com>
2022-07-15 17:58:40 +0900

    docs setAllowedOriginPatters() 옵션을 사용하지 않은 경우를 테스트하기 위해 임시 주석 처리

--- src/main/java/com/doit/study/config/WebsocketConfig.java
+++ src/main/java/com/doit/study/config/WebsocketConfig.java
@@ -19,8 +19,7 @@ public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/websocket")
-                .setAllowedOriginPatterns("*")
+//                .setAllowedOriginPatterns("*")
                 .withSockJS();
     }
-
 }
